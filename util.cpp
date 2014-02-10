8424576a3c934386fd35ceee154e4e1ee48aa134
Evelin <evelincfa@hotmail.com>
2014-02-10 14:35:24 -0200

    Indo palavra por palavra no texto. Porem ainda pega elementos Html

---
+++ util.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  util.cpp
+ *
+ *    Description:  funcoes uteis para este trabalho
+ *
+ *        Version:  1.0
+ *        Created:  02/10/14 14:19:51
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Evelin Amorim, 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include "util.h"
+
+
+void converteParaMinusculo(string& s){
+    /* COnverte uma string para minusculo */
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+}
+
---
+++ util.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  util.h
+ *
+ *    Description:  biblioteca de funcoes uteis
+ *
+ *        Version:  1.0
+ *        Created:  02/10/14 14:20:26
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  evelin amorim, 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void converteParaMinusculo(string& s);
