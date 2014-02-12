4110e48c2c4e7574b88c5ae8b361865a93cd46db
Evelin <evelincfa@hotmail.com>
2014-02-12 11:23:46 -0200

    restaurar arquivos com tokenizador

--- index.cpp
+++ index.cpp
@@ -1,20 +1,21 @@
+
 /*
- * =====================================================================================
- *
- *       Filename:  index.cpp
- *
- *    Description:  Deveria ser o arquivo principal para o indice
- *
- *        Version:  1.0
- *        Created:  02/06/14 23:29:06
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Evelin Amorim, 
- *   Organization:  
- *
- * =====================================================================================
- */
+ * * =====================================================================================
+ * *
+ * * Filename: index.cpp
+ * *
+ * * Description: Deveria ser o arquivo principal para o indice
+ * *
+ * * Version: 1.0
+ * * Created: 02/06/14 23:29:06
+ * * Revision: none
+ * * Compiler: gcc
+ * *
+ * * Author: Evelin Amorim,
+ * * Organization:
+ * *
+ * * =====================================================================================
+ * */
 
 #include "colecao.h"
 #include "CollectionReader.h"
@@ -28,12 +29,14 @@ using namespace RICPNS;
 
 
 int main(int argc,char** argv){
-    string dirEntrada = argv[1];
-    string nomeIndice = argv[2];
+        string dirEntrada = argv[1];
+	    string nomeIndice = argv[2];
 
-    Colecao *col = new Colecao();
+	        Colecao *col = new Colecao();
 
-    col->ler(dirEntrada,nomeIndice);
+		    col->ler(dirEntrada,nomeIndice);
 
-    return 0;
+		        return 0;
 }
+
+
